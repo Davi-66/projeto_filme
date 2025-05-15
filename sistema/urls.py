b0c4c73131bd5d68ed9c7ce57a56b4c86039f588
Davi <drez3nde@gmail.com>
2025-05-15 16:32:17 -0300

    commit do dia 15/05

--- sistema/urls.py
+++ sistema/urls.py
@@ -3,7 +3,7 @@ from sistema import views
 
 # Informa qual será a rota que irá chamar determinada view(função).
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = 'index'),
     
     
     
